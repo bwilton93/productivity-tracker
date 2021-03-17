f3675728baf1debeae0bf56eda3cd87d71c1a045
Barney W <61240093+bwilton93@users.noreply.github.com>
2021-03-17 14:29:32 +0000

    Put script inside a function

--- time-conversion.js
+++ time-conversion.js
@@ -1,22 +1,27 @@
 // Collect start and end time
 let startTime = '13:50:23';
 let endTime = '19:28:92';
+let timeDisplay;
 
-// Split time at colon
-let startTimeSplit = startTime.split(':');
-let endTimeSplit = endTime.split(':');
+function timeDifference() {
+    // Split time at colon
+    let startTimeSplit = startTime.split(':');
+    let endTimeSplit = endTime.split(':');
+    
+    // Convert hours into minutes by multiplying by 60 - the number of minutes in an hour
+    let startMinutes = (Number(startTimeSplit[0])) * 60 + (Number(startTimeSplit[1]));
+    let endMinutes = (Number(endTimeSplit[0])) * 60 + (Number(endTimeSplit[1]));
+    
+    // Calculate actual minutes spent
+    let totalMinutesSpent = endMinutes - startMinutes;
+    
+    // Convert minutes spent into hours and minutes
+    let hoursSpent = Math.floor(totalMinutesSpent / 60);
+    let minutesSpent = totalMinutesSpent % 60;
+    
+    // Display total time in the H:M format
+    timeDisplay = hoursSpent.toString() + "H" + minutesSpent.toString() + "m";
+}
 
-// Convert hours into minutes by multiplying by 60 - the number of minutes in an hour
-let startMinutes = (Number(startTimeSplit[0])) * 60 + (Number(startTimeSplit[1]));
-let endMinutes = (Number(endTimeSplit[0])) * 60 + (Number(endTimeSplit[1]));
-
-// Calculate actual minutes spent
-let totalMinutesSpent = endMinutes - startMinutes;
-
-// Convert minutes spent into hours and minutes
-let hoursSpent = Math.floor(totalMinutesSpent / 60);
-let minutesSpent = totalMinutesSpent % 60;
-
-// Display total time in the H:M format
-let timeDisplay = hoursSpent.toString() + "H" + minutesSpent.toString() + "m";
+timeDifference(startTime, endTime);
 timeDisplay;
