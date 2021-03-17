02bd30eac16169f3cbb13acb7344ae358d58b77e
Barney W <61240093+bwilton93@users.noreply.github.com>
2021-03-17 21:24:16 +0000

    Added seconds tracking & refactored code
    - Refactored some line lengths to be easier to read.
    - Added seconds tracking to allow MAXIMUM PRECISION!!!!

--- script.js
+++ script.js
@@ -4,22 +4,33 @@ let timeDisplay;
 let ended = false;
 
 function startButton() {
+  // Extract time at start of program
   startDate = new Date();
-  document.getElementById("start-time").innerHTML = "Start time: " + startDate.getHours() + ":" + startDate.getMinutes() + ":" + startDate.getSeconds();
+  document.getElementById("start-time").innerHTML = "Start time: " + startDate.getHours() + 
+    ":" + startDate.getMinutes() + ":" + startDate.getSeconds();
 }
 
 function endButton() {
+  // Extract time at end of program
   endDate = new Date();
-  document.getElementById("end-time").innerHTML = "End time: " + endDate.getHours() + ":" + endDate.getMinutes() + ":" + endDate.getSeconds();
+  document.getElementById("end-time").innerHTML = "End time: " + endDate.getHours() +
+    ":" + endDate.getMinutes() + ":" + endDate.getSeconds();
 
-  // Run function
-  timeDifference(startDate, endDate);
+  // Run comparison function
+  timeCalculation(startDate, endDate);
 
-  // Display time spent in D:H:m format
+  // Display time spent in D:H:M:S format
   document.getElementById("display-time").innerHTML = "Time elapsed: " + timeDisplay;
 }
-  
-function timeDifference() {
+
+function clearButton() {
+  // Clear displayed information
+  document.getElementById("start-time").innerHTML = "Start time: N/A";
+  document.getElementById("end-time").innerHTML = "End time: N/A";
+  document.getElementById("display-time").innerHTML ="Time elapsed: 0D:0H:0M";
+}
+
+function timeCalculation() {
   // Get difference in time in milliseconds
   let millisecondsSpent = endDate.getTime() - startDate.getTime();
   
@@ -31,9 +42,22 @@ function timeDifference() {
   let daysSpent = Math.floor(totalHoursSpent / 24);
   let hoursSpent = Math.floor(totalHoursSpent % 24);
   let minutesSpent = Math.floor(totalMinutesSpent % 60);
+
+  // Extract seconds from time and invert if past one minute (Seconds value = negative)
+  // 0 minutes 24 seconds & 1 minute 36 seconds
+  // 1 minute increments as per above
+  // 0 - 24 = -24 seconds
+  // 60 + -24 = 36 seconds
+  // Final display would read 1M:36S
+  let secondsSpent = endDate.getSeconds() - startDate.getSeconds();
   
+  if (endDate.getSeconds() < startDate.getSeconds()) {
+    secondsSpent = 60 + secondsSpent;
+  }
+
   // Display total time in the H:M format
-  timeDisplay = daysSpent.toString() + "D:" + 
-  hoursSpent.toString() + "H:" + 
-  minutesSpent.toString() + "M";
+    timeDisplay = daysSpent.toString() + "D:" + 
+      hoursSpent.toString() + "H:" + 
+      minutesSpent.toString() + "M:" +
+      secondsSpent.toString() + "S";
 }
